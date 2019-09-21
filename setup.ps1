cdbc0e5badbac57e7b10d4054148f7bade6604e7
Craig Mayhew <craig@mayhew.io>
2019-09-21 15:50:41 +0100

    Added specific choco source https://chocolatey.org/api/v2

--- setup.ps1
+++ setup.ps1
@@ -23,6 +23,6 @@ Get-AppxPackage *xboxapp* | Remove-AppxPackage
 Get-AppxPackage *zunemusic* | Remove-AppxPackage
 Get-AppxPackage *zunevideo* | Remove-AppxPackage
 
-choco install -y --force 7zip.install docker-cli firefox notepadplusplus.install slack vscode winmerge winscp wsl-ubuntu-1804
+choco install -y --force --source="'.;https://chocolatey.org/api/v2'" 7zip.install docker-cli firefox notepadplusplus.install slack vscode winmerge winscp wsl-ubuntu-1804
 
 Set-ItemProperty 'HKCU:\Control Panel\Colors' -Name Background -Value "0 0 0"
